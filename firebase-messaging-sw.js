3c3fce73a101e8a6e7b2abcde782e948bd367ec4
alexcont80 <alexcont80@gmail.com>
2025-08-01 17:53:31 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,26 @@
 // firebase-messaging-sw.js
 
-importScripts('https://www.gstatic.com/firebasejs/10.12.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDdYX2aKspn_1FYfFOvq9DjOfzNi4IlLOI",
+  apiKey: "AIzaSyBAGBm881emvR65_mpLP9POByLHKc7lV7s",
   authDomain: "coffeepwa.firebaseapp.com",
   projectId: "coffeepwa",
-  storageBucket: "coffeepwa.appspot.com",
+  storageBucket: "coffeepwa.firebasestorage.app",
   messagingSenderId: "916692141186",
-  appId: "1:916692141186:web:esempioAppId123",
-  vapidKey: "BKsfATXYKByaWySUsFXW1w17KPeCQhs5-os1aGgPOiCKFPEgwos3ZGT50Z1klOu_kNaKItLwGhOtvbjftzd3VfA"
+  appId: "1:916692141186:web:14b47c70e7bab12f40a09b",
+  measurementId: "G-YMQGDGZ2TV"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Messaggio in background:', payload);
+  console.log('[firebase-messaging-sw.js] Messaggio ricevuto in background:', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'icon-192.png'
+    icon: '/coffee-pwa/icon-192.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
